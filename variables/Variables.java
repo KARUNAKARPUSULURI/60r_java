6175d54476eb2aaaa7da4a27b0c48ad560e330b8
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-07-15 08:51:46 +0530

    variables

---
+++ variables/Variables.java
@@ -0,0 +1,18 @@
+class Variables{
+	String collegeName;
+	int a;
+	int b;
+	boolean c;
+	public void displayInfo(int a){
+		String stu_name = "karunakar";
+		System.out.println("c" + c);
+	}
+	public static void main(String[] args){
+		Variables v1 = new Variables();
+		int age = 30;	
+		v1.displayInfo(30);
+		System.out.println(v1.collegeName);
+		System.out.println(v1.a);
+	}
+
+}
